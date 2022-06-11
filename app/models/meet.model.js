5925538ebf24a8fcf5a91840c99dad21bf3692d4
manishmg3994 <manishgautammg7@gmail.com>
2022-06-11 11:22:26 +0530

    step 2st

---
+++ app/models/meet.model.js
@@ -0,0 +1,11 @@
+module.exports = mongoose => {
+    const Meet = mongoose.model(
+        "Meet",
+        mongoose.Schema({
+            name: String, // session name
+            meetingid: String, // meeting id
+            sessionid: String, // socket id
+        }, { timestamps: true })
+    );
+    return Meet;
+};
